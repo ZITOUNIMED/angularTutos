adc4bf982962fca9b896011dd3aace22f2de39c0
Zitouni mohamed <zitouni.mohamed.email@gmail.com>
2018-12-14 14:54:12 +0100

    init project and begin routing tuto

---
+++ src/app/tuto-routing/tuto-routing.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-tuto-routing",
+  templateUrl: "./tuto-routing.component.html",
+  styleUrls: ["./tuto-routing.component.css"]
+})
+export class TutoRoutingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
